89f27d8de032534005358f9584f49ca53bb7a0a5
laudeam <laurent.deambrogio@gmail.com>
2023-02-12 15:16:14 +0100

    Clean up

--- src/main.js
+++ src/main.js
@@ -3,26 +3,20 @@ import { Input } from './input.js';
 import { LONGUEUR, HAUTEUR } from './constants.js';
 
 
-
-let padding = 20; //pixels
-
 let canvas = document.getElementById("my-canvas");
+canvas.width = LONGUEUR;
+canvas.height = HAUTEUR;
 
-
+let id_animation_frame;
 const run = () => {
     game.update();
     game.draw();
     id_animation_frame = window.requestAnimationFrame(run);
 };
 
-let id_animation_frame;
-
 let game = Game.create(canvas);
 
-setTimeout(() =>{ run(); }, 1000);
-
-canvas.width = LONGUEUR;
-canvas.height = HAUTEUR;
+setTimeout(() =>{ run(); }, 1000); //TODO : find better solution to wait for assets (images,...) to load
 
 Input.applyEventHandlers(canvas);
 
